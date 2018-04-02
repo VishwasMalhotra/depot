8d76114edbf5579af0455a82f24a8074fa33ada0
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2018-04-02 18:25:31 +0530

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,48 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+# . . .
+Product.create!(title: 'Introduction to Ruby.',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'sample_image.jpg',
+price: 22.89)
+
+Product.create!(title: 'Introduction to Ruby.',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'sample_image.jpg',
+price: 22.89)
+
+Product.create!(title: 'Introduction to Ruby.',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'sample_image.jpg',
+price: 22.89)
+
+Product.create!(title: 'Introduction to Ruby.',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'sample_image.jpg',
+price: 22.89)
