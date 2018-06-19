1dc125ca9136fd11bb57f106c4f3007a23bdfc3a
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2018-06-19 20:25:14 +0530

    Image Validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,14 @@
 class Image < ApplicationRecord
   attr_accessor :picture
   belongs_to :products, optional: true
+
+  validate :uploaded_images
+
+@@image_count = 0
+  def uploaded_images
+    @@image_count += 1
+    errors.add(:base, "Cannot upload more than 3 images.") if @@image_count > 3
+  end
+
 end
+
