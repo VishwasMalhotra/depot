320c487e1c7356a5d9fce6420cd576be8d7ac5de
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2018-06-27 15:22:56 +0530

    Mailer and I18N Exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :orders
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Not a valid Email Address." }
 
+
   def admin?
     role.eql?('admin')
   end
