8d85719a7c36262adb676755c76a453203c60b93
VishwasMalhotra <vishwasmalhotra14@gmail.com>
2018-06-27 17:00:41 +0530

    Changes as per the review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role: [ :user, :admin ]
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
   validates :name, presence: true, uniqueness: true
